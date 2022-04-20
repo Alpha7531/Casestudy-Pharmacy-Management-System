607b5aba5c389902c77ada6e9e6b77ca6682ca5b
Alpha7531 <sammurari17@gmail.com>
2022-04-21 00:09:12 +0530

    adding all microservices in git repo

---
+++ Supplier-Inventory/src/main/java/com/samuel/SupplierInventory/SupplierInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.SupplierInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierInventoryApplication.class, args);
+	}
+
+}
---
+++ Supplier-Inventory/src/test/java/com/samuel/SupplierInventory/SupplierInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.SupplierInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
